caa07b064a3d66e29967e7599fda3e80224ec142
azizhadiid <azizalhadiid88@gmail.com>
2025-03-02 14:33:36 +0700

    TYPE DAN ANNOTATION

--- build/js/index.js
+++ build/js/index.js
@@ -1,2 +1,40 @@
 "use strict";
-console.log('Hello World 23');
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+let say;
+say = "Hello";
+console.log(say);
+function tambah(a, b) {
+    return a + b;
+}
+console.log(tambah(1, 2));
+let mobil;
+mobil = {
+    merk: "Toyota",
+    tahun: 2020,
+};
+console.log(mobil.merk);
+function contoh() {
+    return function (target, propertyKey, descriptor) {
+        console.log("Contoh dijalankan");
+    };
+}
+class Example {
+    method() {
+        console.log("Method dipanggil");
+    }
+}
+__decorate([
+    contoh(),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], Example.prototype, "method", null);
+new Example().method();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('Hello World 23');
+let say: string;
+say = "Hello";
+console.log(say);
+
+function tambah(a: number, b: number): number {
+    return a + b;
+}
+console.log(tambah(1, 2));
+
+let mobil: {
+    merk: string; tahun: number
+};
+mobil = {
+    merk: "Toyota",
+    tahun: 2020,
+};
+console.log(mobil.merk);
+
+function contoh(): MethodDecorator {
+    return function (
+        target: Object,
+        propertyKey: string | symbol,
+        descriptor: PropertyDescriptor
+    ) {
+        console.log("Contoh dijalankan");
+    };
+}
+
+class Example {
+    @contoh()
+    method() {
+        console.log("Method dipanggil");
+    }
+}
+
+new Example().method();
