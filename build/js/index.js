217b015e041f4dc66b54943dee30a99917d1fd1c
azizhadiid <azizalhadiid88@gmail.com>
2025-03-07 17:56:18 +0700

    READONLY PROPERTY

--- build/js/index.js
+++ build/js/index.js
@@ -1,79 +1,28 @@
 "use strict";
-// access modifier
-// 1. private -> hanya bisa diacess oleh class itu sendiri
-// 2. public -> bisa diacess oleh semua class
-// 3. protected -> bisa diacess oleh class itu sendiri dan class turunannya saja
-// 4. tanfa definisi -> berlaku defaulat yaitu public
-class Account {
-    constructor(id, name, balance) {
-        this._id = id;
-        this._name = name;
-        this._balance = balance;
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
     }
-    get balance() {
-        return this._balance;
-    }
-    set balance(value) {
-        this._balance = value;
-    }
-    get name() {
-        return this._name;
-    }
-    set name(value) {
-        this._name = value;
-    }
-    get id() {
-        return this._id;
-    }
-    set id(value) {
-        this._id = value;
+    getArea() {
+        return Math.PI * this.radius * this.radius;
     }
 }
-let account = new Account(1, "abc", 1000);
-console.log(account.balance);
-class Bnatang {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    showInfo() {
-        console.log(`Name: ${this.name}, Age: ${this.age}`);
-    }
-}
-let binatang = new Bnatang("abc", 10);
-binatang.name = "TEsting Binatang";
-binatang.showInfo();
-class Animal {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    showInfo() {
-        console.log(`Name: ${this.name}, Age: ${this.age}`);
-    }
-}
-// let animal = new Animal("abc", 10);
-// animal.name = "Kucing";
-class Dog extends Animal {
-    constructor(name, age, breed) {
-        super(name, age);
-        this.breed = breed;
-    }
-    showInfo2() {
-        super.showInfo();
-        console.log(`Breed: ${this.breed} dan Age: ${this.age} dan Name: ${this.name}`);
-    }
-}
-let dog = new Dog("abc", 10, "Lab");
-dog.showInfo2();
-class Manusia {
-    constructor(nama) {
-        this.nama = nama;
-    }
-    setNama(nama) {
-        this.nama = nama;
-    }
-}
-let mhs = new Manusia("abc");
-mhs.setNama("Muhammad");
-console.log(mhs.nama);
+let circle = new Circle(10);
+console.log(circle.radius);
+console.log(circle.getArea());
+let person = {
+    name: "John",
+    hobbies: ["Sports", "Cooking"],
+};
+console.log(person.hobbies);
+console.log(person.name);
+let emp1 = {
+    empCode: 1,
+    empName: "John",
+};
+console.log(emp1);
+let point = {
+    x: 10,
+    y: 20,
+};
+console.log(point);
--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,66 @@
-// access modifier
-// 1. private -> hanya bisa diacess oleh class itu sendiri
-// 2. public -> bisa diacess oleh semua class
-// 3. protected -> bisa diacess oleh class itu sendiri dan class turunannya saja
-// 4. tanfa definisi -> berlaku defaulat yaitu public
+class Circle {
+    readonly radius: number;
+    constructor(radius: number) {
+        this.radius = radius;
+    }
 
-class Account {
-    private _id: number;
-    private _name: string;
-    private _balance: number;
-    constructor(id: number, name: string, balance: number) {
-        this._id = id;
-        this._name = name;
-        this._balance = balance;
-    }
-    public get balance(): number {
-        return this._balance;
-    }
-    public set balance(value: number) {
-        this._balance = value;
-    }
-    public get name(): string {
-        return this._name;
-    }
-    public set name(value: string) {
-        this._name = value;
-    }
-    public get id(): number {
-        return this._id;
-    }
-    public set id(value: number) {
-        this._id = value;
+    getArea() {
+        return Math.PI * this.radius * this.radius;
     }
 }
 
-let account = new Account(1, "abc", 1000);
-console.log(account.balance);
+let circle = new Circle(10);
+console.log(circle.radius);
+console.log(circle.getArea());
 
-class Bnatang {
-    public name: string;
-    public age: number;
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
-    showInfo() {
-        console.log(`Name: ${this.name}, Age: ${this.age}`);
-    }
+// readonly dalan interface
+interface Person {
+    readonly name: string;
+    readonly hobbies: ReadonlyArray<string>;
 }
 
-let binatang = new Bnatang("abc", 10);
-binatang.name = "TEsting Binatang";
-binatang.showInfo();
+let person: Person = {
+    name: "John",
+    hobbies: ["Sports", "Cooking"],
+};
+console.log(person.hobbies);
+console.log(person.name);
 
-class Animal {
-    protected name: string;
-    protected age: number;
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
-    showInfo() {
-        console.log(`Name: ${this.name}, Age: ${this.age}`);
-    }
+// person.hobbies.push("Sports");
+// person.name = "Jane";
+
+// readonly untuk interface type
+
+interface IEmployee {
+    empCode: number;
+    empName: string;
 }
 
-// let animal = new Animal("abc", 10);
-// animal.name = "Kucing";
+let emp1: Readonly<IEmployee> = {
+    empCode: 1,
+    empName: "John",
+};
 
-class Dog extends Animal {
-    public breed: string;
-    constructor(name: string, age: number, breed: string) {
-        super(name, age);
-        this.breed = breed;
-    }
-    showInfo2() {
-        super.showInfo();
-        console.log(
-            `Breed: ${this.breed} dan Age: ${this.age} dan Name: ${this.name}`
-        );
-    }
-}
+console.log(emp1);
 
-let dog = new Dog("abc", 10, "Lab");
-dog.showInfo2();
+// emp1.empCode = 2;
+// emp1.empName = "Jane";
 
-class Manusia {
-    nama: string;
+// readonly untuk generik
 
-    constructor(nama: string) {
-        this.nama = nama;
-    }
+type ReadonlyPoint<T> = {
+    readonly [K in keyof T]: T[K];
+};
 
-    setNama(nama: string) {
-        this.nama = nama;
-    }
-}
+type Point = {
+    x: number;
+    y: number;
+};
 
-let mhs = new Manusia("abc");
-mhs.setNama("Muhammad");
-console.log(mhs.nama);
+type ReadonlyPoint2D = ReadonlyPoint<Point>;
+let point: ReadonlyPoint2D = {
+    x: 10,
+    y: 20,
+};
+
+console.log(point);
